2b1d0068cee3630257dc2289556841617470720e
Takuya Nishio <littlecub240@gmail.com>
2023-08-12 22:57:15 +0900

    [add] Add Setting EventNames

--- app/models/simple_pub_sub.rb
+++ app/models/simple_pub_sub.rb
@@ -1,6 +1,18 @@
 module SimplePubSub
   class InvalidEventError < StandardError; end
   class InvalidPayloadError < StandardError; end
+
+  @event_names_module = nil
+  
+  class << self
+    attr_writer :event_names_module
+  end
+
+  def self.event_names
+    return [] unless @event_names_module
+
+    @event_names_module.constants.map { |const_name| @event_names_module.const_get(const_name) }
+  end
   
   def self.subscriptions
     Thread.current[:simple_pub_sub_subscriptions] ||= Hash.new { |hash, key| hash[key] = [] }
@@ -18,20 +30,21 @@ module SimplePubSub
   end
 
   def self.subscribe(event_name, subscriber, payload_klass)
-    raise InvalidEventError, "Unknown event name: #{event_name}" unless EventNames.const_defined?(event_name.upcase)
+    raise InvalidEventError, "Unknown event name: #{event_name}" unless event_names.include?(event_name)
     
     subscriptions[event_name] << { subscriber: subscriber, payload_klass: payload_klass }
   end
 
   def self.publish(event_name, payload)
     return if muted_events.include?(event_name)
-    raise InvalidEventError, "Unknown event name: #{event_name}" unless EventNames.const_defined?(event_name.upcase)
+    raise InvalidEventError, "Unknown event name: #{event_name}" unless event_names.include?(event_name)
 
     subscriptions[event_name].each do |subscription|
       expected_klass = subscription[:payload_klass]
       unless payload.class.to_s == expected_klass.to_s
         raise InvalidPayloadError, "Expected payload of type #{expected_klass}, got #{payload.class}"
       end
+      # Todo Interface
       payload.validate! if payload.respond_to?(:validate!)
       subscription[:subscriber].call(payload)
     end
--- config/initializers/simple_pub_sub_setup.rb
+++ config/initializers/simple_pub_sub_setup.rb
@@ -1,4 +1,5 @@
 Rails.configuration.after_initialize do
+  SimplePubSub.event_names_module = EventNames
   SimplePubSub.subscribe(:user_created, Subscribers::UserCreated.new, Payloads::UserCreated)
   SimplePubSub.subscribe(:user_destroyed, Subscribers::UserDestroyed.new, Payloads::UserDestroyed)
 end
